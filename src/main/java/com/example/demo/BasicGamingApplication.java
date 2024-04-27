0fcb7172010ab3dd8b67e54e12e60bfa6078b846
adarsh <a1@gmail.com>
2024-04-27 16:26:22 +0530

    basic spring functionalities added like beans,context

--- src/main/java/com/example/demo/BasicGamingApplication.java
+++ src/main/java/com/example/demo/BasicGamingApplication.java
@@ -1,15 +1,26 @@
 package com.example.demo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.example.demo.game.GameRunner;
-import com.example.demo.game.MarioGame;
-import com.example.demo.game.SuperContra;
+import com.example.demo.game.GamingConsole;
+//import com.example.demo.game.MarioGame;
+//import com.example.demo.game.SuperContra;
 
 public class BasicGamingApplication {
-   public static void main(String args[]) {
-	      //var game = new MarioGame();
-	      SuperContra game = new SuperContra();
-	      GameRunner gamerunner = new GameRunner(game);
-	      System.out.println("my name is adarsh");
-	      gamerunner.run();
+
+public static void main(String args[]) {
+	      try (var context = new AnnotationConfigApplicationContext(BasicGamingconfig.class)) {
+			context.getBean(GamingConsole.class).up();
+			  //x.up();
+		} catch (BeansException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      
+	      var context1 = new AnnotationConfigApplicationContext(BasicGamingconfig.class);
+	      context1.getBean(GameRunner.class).run();
    }
+
 }
