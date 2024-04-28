4e65b9263ad18f52be6d85fd3564596a45235a8a
adarsh <a1@gmail.com>
2024-04-28 08:51:30 +0530

    PostConstuct and PreDestroy added

--- src/main/java/com/example/demo/example/a1/BasicGamingApplication.java
+++ src/main/java/com/example/demo/example/a1/BasicGamingApplication.java
@@ -15,38 +15,47 @@ import com.example.demo.game.GameRunner;
 import com.example.demo.game.MarioGame;
 import com.example.demo.game.SuperContra;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 
 @Component
-class singleton_class{
-	
-	
+class main_class
+{
+	Dependency_class dependency_class;
+	public main_class(Dependency_class dependency_class) {
+		System.out.println("main_class constructor invoked");
+		this.dependency_class = dependency_class;
+	}
+	@PostConstruct
+	public void fun() {
+		System.out.println("PostConstruct invoked");
+		dependency_class.fun1();
+	}
+	@PreDestroy
+	public void fun2() {
+		System.out.println("pre-destroy invoked");
+	}
 }
 
-@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Component
-class prototype_class{
-	   
+class Dependency_class
+{
+    public void fun1() {
+    	System.out.println("my name is adarsh");
+    }
+	public Dependency_class() {
+		System.out.println("Dependency_class constructor invoked");
+	}
+	
 }
-
-
 @Configuration
 @ComponentScan
 public class BasicGamingApplication {
    public static void main(String args[]) {
 	   System.out.println("BasicGamingApplication invoked");
 	     var context = new AnnotationConfigApplicationContext(BasicGamingApplication.class);
-	     System.out.println(context.getBeansOfType(singleton_class.class));
-	     System.out.println(context.getBeansOfType(singleton_class.class));
-	     System.out.println(context.getBeansOfType(singleton_class.class));
-	     System.out.println(context.getBeansOfType(singleton_class.class));
-	     
-	     System.out.println("prototype_class");
-	     
-	     
-	     System.out.println(context.getBeansOfType(prototype_class.class));
-	     System.out.println(context.getBeansOfType(prototype_class.class));
-	     System.out.println(context.getBeansOfType(prototype_class.class));
-	     
+	     //System.out.println(context.getBean(main_class.class));
+	     Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
    }
 }
